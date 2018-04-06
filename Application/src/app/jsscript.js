baec43ea5a1e2f0ae4fb55ec3a44f6d333c4615f
shadman22 <shadman94@gmail.com>
2018-04-06 12:43:37 +0600

    Home Page Design Complete

---
+++ Application/src/app/jsscript.js
@@ -0,0 +1,94 @@
+
+
+function navbaranimation (){
+  $(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 800) {
+    	$("#navigation").addClass("fixed-top");
+    }
+
+    else if (scroll < 5){
+      	$("#navigation").removeClass("fixed-top");   
+    }
+  })
+})
+}
+
+
+
+$(document).ready(function(){
+    $("#about_us_2").hide();
+});
+$(document).ready(function(){
+    $("#aboutShow").click(function(){
+      $("#about_us_2").show();
+      $("#aboutShow").hide();
+    });
+});
+
+function content_animation(){
+  $(document).ready(function() {
+
+  //window and animation items
+  var animation_elements = $.find('.animation-element');
+  var web_window = $(window);
+
+  //check to see if any animation containers are currently in view
+  function check_if_in_view() {
+    //get current window information
+    var window_height = web_window.height();
+    var window_top_position = web_window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    //iterate through elements to see if its in view
+    $.each(animation_elements, function() {
+
+      //get the element sinformation
+      var element = $(this);
+      var element_height = $(element).outerHeight();
+      var element_top_position = $(element).offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is visible (its viewable if it exists between the viewable space of the viewport)
+      if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+        element.addClass('in-view');
+      } else {
+        element.removeClass('in-view');
+      }
+    });
+
+  }
+
+  //on or scroll, detect elements in view
+  $(window).on('scroll resize', function() {
+      check_if_in_view()
+    })
+    //trigger our scroll event on initial load
+  $(window).trigger('scroll');
+
+});
+}
+
+function scrollsmooth(){
+	$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+        });
+      }
+    }
+  });
+}
